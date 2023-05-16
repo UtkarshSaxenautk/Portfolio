e1ff945910de347be97829ef44871e605ec6f9d9
Utkarsh Saxena <nive54@gmail.com>
2023-05-16 18:22:58 +0530

    minor fixes and resume updated

--- src/components/Containers/contact/Contact.js
+++ src/components/Containers/contact/Contact.js
@@ -71,7 +71,7 @@ const Contact = () => {
                 <div className="row mb-4" > <div className="col-md-4" >
                     <div className="dbox w-100 d-flex align-items-start" >
                         <div className="text" > <p><span>LinkedIn:</span>
-                            <a href="https://www.linkedin.com/in/utkarsh-saxena-5a9034201/" > utkarsh-saxena</a>
+                            <a href="https://www.linkedin.com/in/utkarsh-saxena-5a9034201/" target='_blank' rel="noreferrer" > utkarsh-saxena</a>
                         </p>
                         </div>
                     </div>
@@ -128,10 +128,10 @@ const Contact = () => {
         }
 
         /> </div> </div> <div className="col-md-12" > <div className="form-group" > <input type="submit" defaultValue="Send Message" className="btn btn-primary" /> <div className="submitting" /> </div> </div> </div> </form> <div className="w-100 social-media mt-5" > <h3>Follow me here</h3> <p>
-            <a href="https://github.com/UtkarshSaxenautk" >Github</a>
-            <a href="https://stackoverflow.com/users/19090939/utkarsh-saxena" >StackOverflow</a>
-            <a href="/" >Instagram</a>
-            <a href="https://www.linkedin.com/in/utkarsh-saxena-5a9034201/" >Linkedin</a>
+            <a href="https://github.com/UtkarshSaxenautk" target='_blank' rel="noreferrer" >Github</a>
+            <a href="https://stackoverflow.com/users/19090939/utkarsh-saxena" target='_blank' rel="noreferrer" >StackOverflow</a>
+            <a href="https://www.instagram.com/utkarsh_saxena_utk/"  target='_blank' rel="noreferrer">Instagram</a>
+            <a href="https://www.linkedin.com/in/utkarsh-saxena-5a9034201/" target='_blank' rel="noreferrer" >Linkedin</a>
         </p> </div> </div> </div> <div className="col-lg-6 d-flex align-items-stretch" > <div className="info-wrap w-100 p-5 img" style={
                 {
                 backgroundImage: `url(${utkc})`
